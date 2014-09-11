4f31592e123e7dae09f335a83c41e677f1a6daeb
Jan Krutisch <jan@krutisch.de>
2014-09-11 11:51:48 +0200

    Brace yourselves. REPL is comming.

--- index.js
+++ index.js
@@ -4,12 +4,50 @@ var deferreds = [BufferHandler.load(amen)];
 
 $.when.apply($, deferreds).done(function(){
   console.log('READY FOR TAKEOFF!');
+  installRepl();
 });
 
-window.funkyDrums = function(){
+window.funkydrums = function(){
   BufferHandler.play(amen, {loop: true});
+  return "ain't it funky."
 };
 
-window.goCracy = function(){
+window.gocrazy = function(){
   $(document.body).addClass('gocracy');
+  return "went crazy. happy now?";
 };
+
+window.reset = function(){
+  $(document.body).removeClass('gocracy');
+  return "went crazy. happy now?";
+};
+
+
+function installRepl() {  
+  $REPL = $('#repl');
+  $INPUT = $('#repl-input');
+  
+  function out(text) {
+    $REPL.prepend($('<pre>&nbsp;&nbsp;' + text + '</pre>'));
+    $INPUT.val("").focus();    
+  }
+  
+  $('#repl-input').on('keypress', function(e) {
+    if (e.which == 13) {
+      try {
+        line = $INPUT.val();
+        if(line[line.length - 1] !== ';') {
+          out("Missing Semicolon!");
+          return;
+        }
+        ret_val = eval($INPUT.val());
+        out(ret_val);
+      } catch (e) {
+        console.log(e);
+        out(e);        
+      }
+    }
+  });
+  $('#repl-input').val("").focus();
+}
+
